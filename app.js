93ccc863db3874ce41f5a34f1ba7afab78bb34e2
Mikael Wallin <mikael.x.wallin@gmail.com>
2022-04-04 15:05:32 +0200

    Working api

--- app.js
+++ app.js
@@ -10,13 +10,70 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get("/", (request ,response)=>{
-   headers={"http_status":200, "cache-control":  "no-cache"}
-   body={"status": "available"}
+app.get("/", (request, response)=>{
+   response.set("http_status",200)
+   response.set("cache-control",  "no-cache")
    response.set('Content-Type', 'application/json');
+   body={"status": "available"}
+   response.status(200).send(body)
+})
+
+app.get("/threads", (request, response)=>{
+   body={"threads": 
+   [
+      {"id":"1"}
+      ,{"id":"2"}
+      ,{"id":"3"}
+      ,{"id":"4"}
+   ]}
+   response.status(200).send(body)
+})
+
+app.post("/threads", (request, response)=>{
+   console.log(request.body)
+
+   body={"threads": 
+   [
+      {"id":"1"}
+      ,{"id":"2"}
+      ,{"id":"3"}
+      ,{"id":"4"}
+   ]}
+   response.status(200).send(body)
+})
+
+app.get("/threads/:id", (request, response)=> {
+   console.log(request.params)
+   body={"id":request.params.id}
+   response.status(200).send(body)
+})
+
+app.get("/threads/:id/replies", (request, response)=> {
+   console.log(request.params)
+   body={"id":request.params.id, "replies": [{"id":1, "reply": "any reply"}, {"id":2, "reply": "another reply"}]}
+   response.status(200).send(body)
+})
+
+app.post("/threads/:id/replies", (request, response)=> {
+   console.log(request.params)
+   console.log(request.body)
+   body={"id":request.params.id, "replies": [{"id":1, "reply": "any reply"}, {"id":2, "reply": "another reply"}]}
+   response.status(200).send(body)
+})
+
+app.post("/threads/:threadId/replies/:replyId/like", (request, response)=> {
+   console.log(request.params)
+   body={"threadId":request.params.threadId, "replyId": request.params.replyId}
+   response.status(200).send(body)
+})
+
+app.delete("/threads/:threadId/replies/:replyId/like", (request, response)=> {
+   console.log(request.params)
+   body={"threadId":request.params.threadId, "replyId": request.params.replyId}
    response.status(200).send(body)
 })
 
 app.listen(PORT , ()=>{
      console.log(`STARTED LISTENING ON PORT ${PORT}`)
 })
+
