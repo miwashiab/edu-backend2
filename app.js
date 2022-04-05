86be732b80d64d6f9792644b37c32c198803516e
Mikael Wallin <mikael.x.wallin@gmail.com>
2022-04-05 12:52:37 +0200

    Removed bug

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const cors = require("cors")
 const express = require("express")
+const mongoose = require("mongoose");
 const bodyParser = require('body-parser')
+
+mongoose.connect("mongodb+srv://test-user:test-user-password@cluster0.oyfx8.mongodb.net/test-db?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Thread = require("./model/threads");
+const Reply = require("./model/replies");
+const Like = require("./model/likes");
+const User = require("./model/users");
+const { response } = require("express");
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -16,6 +28,8 @@ app.get("/", (request, response)=>{
    response.set('Content-Type', 'application/json');
    body={"status": "available"}
    response.status(200).send(body)
+
+   User.create
 })
 
 app.get("/threads", (request, response)=>{
@@ -71,6 +85,33 @@ app.delete("/threads/:threadId/replies/:replyId/like", (request, response)=> {
    console.log(request.params)
    body={"threadId":request.params.threadId, "replyId": request.params.replyId}
    response.status(200).send(body)
+})
+
+
+//curl -X POST http://localhost:3001/users -H "Content-Type: application/json" -d "{\"username\":\"nisse\",\"password\":\"password\"}"
+//Create
+app.post("/users", (request, response) => {
+   console.log(request.body)
+   let user = new User(request.body)
+   user.save()
+   response.status(200).send(request.body)
+})
+
+app.get("/users/:id", (request, response)=> {
+   console.log(request.params.id)
+   try {
+      User.findById(request.params.id, (err, user) => {
+         if (err) throw error;
+         if (user) {
+            response.status(200).json(user)
+         } else {
+            response.status(404).send("Not found")
+         }
+      })
+   } catch (e) {
+      console.error(e)
+      response.status(400).send("Bad request")
+   }
 })
 
 app.listen(PORT , ()=>{
